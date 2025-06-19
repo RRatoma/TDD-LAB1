1e4e81d14de8122c6b166183fed28e2e4e6efc56
Thomas Dragoi <thomasdragoi@yahoo.de>
2025-06-19 19:03:20 +0200

    Test

---
+++ src/find_spaceship.js
@@ -0,0 +1,16 @@
+// spec/findSpaceship.spec.js
+const findSpaceship = require('../src/findSpaceship');
+
+describe('findSpaceship', () => {
+  it('should return "Spaceship lost forever." if there is no spaceship on the map', () => {
+    const map =
+      "..........\n" +
+      "..........\n" +
+      "..........\n" +
+      "..........\n" +
+      "..........\n" +
+      "..........";
+
+    expect(findSpaceship(map)).toBe("Spaceship lost forever.");
+  });
+});
