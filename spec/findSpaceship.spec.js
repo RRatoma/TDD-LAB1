70b8b2dbb12ff0c7e16d79500fade51e0b32e1f1
Thomas Dragoi <thomasdragoi@yahoo.de>
2025-06-20 03:19:14 +0200

    0.0

--- spec/findSpaceship.spec.js
+++ spec/findSpaceship.spec.js
@@ -21,6 +21,30 @@ describe("findSpaceship", function () {
     "..........";
 
   expect(findSpaceship(map)).toEqual([7, 2]);
+  });
+
+  it("should return [0, 0] if the spaceship is at bottom left corner", function () {
+  const map =
+    "..........\n" +
+    "..........\n" +
+    "..........\n" +
+    "..........\n" +
+    "..........\n" +
+    "X.........";
+
+  expect(findSpaceship(map)).toEqual([0, 0]);
+});
+
+it("should return [9, 5] if the spaceship is at top right corner", function () {
+  const map =
+    ".........X\n" +
+    "..........\n" +
+    "..........\n" +
+    "..........\n" +
+    "..........\n" +
+    "..........";
+
+  expect(findSpaceship(map)).toEqual([9, 5]);
 });
 
 });
